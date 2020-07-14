fbf1c191353225c7e78a4a5333dc68f886428f0b
user <dav4awss@gmail.com>
2020-07-14 19:16:35 +0300

    20200714_19_16

---
+++ KlasesDarbi/20200713-DAY9-10/dev/MySecondConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MySecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+     	{	
+            int num = 123;
+            int num2 = 321;
+            int num3 = num+num2;
+
+            
+            Console.WriteLine("Cipars");
+            Console.WriteLine(num3);
+
+
+            
+	        var text = "EDIBO";
+            Console.WriteLine($"Hello World! {text} RTU {num}");
+        }
+    }
+}
