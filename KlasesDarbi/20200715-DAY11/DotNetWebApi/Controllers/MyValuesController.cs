17442d7c61492583e722d59bd476c8375965647c
user <dav4awss@gmail.com>
2020-07-15 18:42:56 +0300

    20200715_18_42

---
+++ KlasesDarbi/20200715-DAY11/DotNetWebApi/Controllers/MyValuesController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetWebApi.Controllers
+{
+    [ApiController]
+    [Route("value")]
+    public class MyValuesController : ControllerBase
+    {
+        // Get api/values
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAction(){
+            return new string[] {"July", "2020", "EDIBO", "RTU"};
+        }
+    }
+}
