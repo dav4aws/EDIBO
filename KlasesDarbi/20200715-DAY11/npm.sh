4e7a4a5cfaeac3df6770fa7f3e0f97b00e84a42a
user <dav4awss@gmail.com>
2020-07-16 14:34:55 +0300

    20200716_14_34

--- KlasesDarbi/20200715-DAY11/npm.sh
+++ KlasesDarbi/20200715-DAY11/npm.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
-echo Installing NODE................
-export NVM_DIR="$HOME/.nvm" && (
- git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
- cd "$NVM_DIR"
- git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
+#echo Installing NODE................
+#export NVM_DIR="$HOME/.nvm" && (
+# git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+# cd "$NVM_DIR"
+# git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+#) && \. "$NVM_DIR/nvm.sh"
+#echo ----------------------- DONE -------------------------
+#echo Installing NPM................
+#`nvm i 12`
+#echo ----------------------- DONE -------------------------
+#echo Installing................
+#`npm install -g @angular/cli`
+#echo ----------------------- DONE -------------------------
+
+ng new data
+
+
+cd data
+mv node_modules ../FirstAngular/
 echo ----------------------- DONE -------------------------
-echo Installing NPM................
-`nvm i 12`
+ng add @angular/material
 echo ----------------------- DONE -------------------------
-echo Installing................
-`npm install -g @angular/cli`
-echo ----------------------- DONE -------------------------
-
-`ng ng new data`
-`cd data`
-`mv node_modules ../FirstAngular/`
-echo ----------------------- DONE -------------------------
-
-
-
-
+rmdir data
